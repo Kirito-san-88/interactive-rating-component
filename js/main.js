6e9dc8f20d54d8b1b51dff21dcf7bb305b5aa590
Kirito-san-88 <vincentbsr88@gmail.com>
2022-09-15 10:51:14 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    let inputRating = document.querySelectorAll(".input"); // get all input
+    let button = document.getElementById("button"); // button submit
+    let spanRating = document.getElementById("rating"); // span where rating will be displayed
+    let endRating = document.querySelector(".end-rating");// for display the thank you card
+    let startRating = document.querySelector(".start-rating");
+
+    for (let i = 0; i < inputRating.length; i++) {
+        inputRating[i].addEventListener('click', () => {
+            let rating = inputRating[i].innerHTML;
+
+            for (let i = 0; i < inputRating.length; i++) {
+                inputRating[i].classList.contains("select");
+               inputRating[i].classList.remove("select");
+            }
+            inputRating[i].classList.add("select");
+
+            button.addEventListener('click', () => {
+                startRating.style.display = "none";
+                endRating.style.display = "block";
+                spanRating.innerHTML = rating;
+            });
+        });
+    };
+});   
